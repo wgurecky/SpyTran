17797aeb42283f9282241d25e9924c83b9f68145
mcoveri <william.gurecky@gmail.com>
2015-10-05 23:58:38 -0500

    updated 1d sn

--- src/sn1D.py
+++ src/sn1D.py
@@ -4,22 +4,10 @@
 #
 # Sweeps through energy, direction, and space on a 1D mesh
 #
-# Class hiarchy:
-#
-# - Domain
-#   - sub-domain
-#       - mesh-material region
-#           - cells
 #
 
 import numpy as np
 import materials.materialMixxer as mx
-
-# STANDARD ORDINATES AND FLUX WEIGHTS FOR STD QUADRATURE SET
-sN2w = np.array([1.0, 1.0])
-sN2mu = np.array([0.5773502691, -0.5773502691])
-sN4w = np.array([0.34785, 0.65214, 0.65214, 0.34785])
-sN4mu = np.array([0.86113, 0.33998, -0.33998, -0.86113])
 
 
 class Domain(object):
@@ -80,38 +68,50 @@ class SubDomain(object):
 
 class Mesh1Dsn(object):
     def __init__(self, bounds, deltaX, material, **kwargs):
-        self.sNorder = kwargs.pop("sNorder", 2)
-        self._setOrds()
+        # sNorder = kwargs.pop("sNorder", 2)
         self.deltaX = deltaX
+        self.depth = 0  # scattering source iteration
         # initilize all cells in the mesh.
-        self.cells = []
+        self.cells, self.cellXPos = [], []
         for i, pos in enumerate(np.arange(bounds[0], bounds[1] + deltaX, deltaX)):
-            self.cellPos[i] = pos
+            self.cellXPos.append(pos)
             self.cells.append(Cell1DSn())
-
+        self.cellXPos = np.array(self.cellXPos)
+        #
         self.totalXs = material.macroProp['Ntotal']
         self.skernel = material.macroProp['Nskernel']
-        self.qin = []  # scattering source
-        pass
-
-    def _setOrds(self):
-        if self.sNorder == 2:
-            self.mu = np.array([-1., 1.])
-        else:
-            pass
 
     def buildCells(self):
         # save nearest neighbor information in preperation to generalize to 2D,
         # unstructured meshes.
         pass
 
+    def makeSweepPattern(self):
+        """
+        returns a list of cell ids
+        list ordered by sweep direction.
+          in 1D returns 1D array
+
+        ex:
+            ------> sweep dir
+        cellIDs = [2, 4, 1, 3, 5, 6]
+        cellXPos = [1, 2, 3, 4, 5, 6]
+        order by X dist from min(cellXPos).  Need to use dist formula in 2 and 3D.
+        return = [2, 4, 1, 3, 5, 6]
+        """
+        pass
+
     def sweepMesh(self, bcLeft, bcRight):
         """
-        March through the cells in the mesh, update the ordinate fluxes as we go
-        Specify ordinate direction to travel in.  Go in +mu dir, then back
-        in the -mu direction (if necissary) untill convergence
+        Outer space iterations:
+            March through the cells in the mesh, update the ordinate fluxes as we go
+            Specify ordinate direction to travel in.  Go in +mu dir, then back
+            in the -mu direction (if necissary) untill convergence.
+
+        Inner sN - energy iterations:
+            perform scattering source inner iterations on each cell untill convergence.
+            distribute neutrons in angle and energy.
         """
-        # Sweep angle and energy to obtain qin (inscatter source)
         # Sweep space
         converged, i = False, 0
         while not converged:
@@ -120,23 +120,44 @@ class Mesh1Dsn(object):
             i += 1
             if i > 3:
                 converged = True
+        self.depth += 1  # increment scattering source iter
+        self._addOrdFlux()
 
-    def _sweepDir(self, o, bc):
-        """
-        o is either 1 or 2 in 1D
-        1 is left cell edge,  2 is right edge
-        """
-        # note len(self.ordFlux) == len(self.mu)
-        lastCellFaceVal = bc
+    def _addOrdFlux(self):
+        '''
+        add Nth collided ord flux to running total
+        '''
         for cell in self.cells:
-            cell.ordFlux[:, o, :] = lastCellFaceVal
-            cell.ordFlux[:, 0, :] = (cell.ordFlux[:, o, :] + self.deltaX * cell.qin / (2. * np.abs(self.mu))) / \
+            cell.totOrdFlux += cell.ordFlux
+
+    def _sweepDir(self, f, bc):
+        """
+        f is either 1 or 2 in 1D
+        f stands for face
+        1 is left cell face,  2 is right face
+        """
+        lastCellFaceVal = bc  # set boundary condition on flux at edge
+        if f == 2:
+            cellList = reversed(self.cells)
+        else:
+            cellList = self.cells
+        for cell in cellList:
+            if cell.boundaryCond is not None:
+                # Set cell boundary condition
+                pass
+            else:
+                # Interior cell
+                cell.ordFlux[:, f, :] = lastCellFaceVal
+            # Sweep angle and energy within the cell to update qin (inscatter source)
+            cell.sweepOrd(self.depth)
+            # Step through space
+            cell.ordFlux[:, 0, :] = (cell.ordFlux[:, f, :] + self.deltaX * cell.qin / (2. * np.abs(self.mu))) / \
                 (1. + self.totalXs * self.deltaX / (2. * np.abs(self.mu)))
-            if o == 1:
-                cell.ordFlux[:, 2, :] = 2. * cell.ordFlux[:, 0, :] - cell.ordFlux[:, o, :]
+            if f == 1:
+                cell.ordFlux[:, 2, :] = 2. * cell.ordFlux[:, 0, :] - cell.ordFlux[:, f, :]
                 lastCellFaceVal = cell.ordFlux[:, 2, :]
-            if o == 2:
-                cell.ordFlux[:, 1, :] = 2. * cell.ordFlux[:, 0, :] - cell.ordFlux[:, o, :]
+            if f == 2:
+                cell.ordFlux[:, 1, :] = 2. * cell.ordFlux[:, 0, :] - cell.ordFlux[:, f, :]
                 lastCellFaceVal = cell.ordFlux[:, 1, :]
 
 
@@ -154,19 +175,44 @@ class Cell1DSn(object):
     mu=cos(theta)
     in S2, bin by 90deg chunks
     """
+    # STANDARD ORDINATES AND FLUX WEIGHTS FOR STD QUADRATURE SET
+    sNwDict = {2: np.array([1.0, 1.0]),
+               4: np.array([0.34785, 0.65214, 0.65214, 0.34785])
+               }
+    sNmuDict = {2: np.array([0.5773502691, -0.5773502691]),
+                4: np.array([0.86113, 0.33998, -0.33998, -0.86113])
+                }
 
-    def __init__(self, source, nGroups=10, legOrder=8, sNords=2, **kwargs):
+    def __init__(self, xpos, nGroups=10, legOrder=8, sNords=2, **kwargs):
+        self.faceNormals = np.array([-1, 1])
+        self.centroid = xpos
         # store cell centered, and cell edge fluxes.  Store as
         # len(groups)x3xlen(sNords) matrix.
-        self.src = source  # fission or previous scattering source
-        self.sNords = sNords
-        self.legOrder = legOrder
-        self.nG = nGroups
+        self.sNords = sNords      # number of discrete dirs tracked
+        self.wN = self.sNwDict[sNords]     # quadrature weights
+        self.sNmu = self.sNmuDict[sNords]  # direction cosines
+        self.maxLegOrder = legOrder  # remember to range(maxLegORder + 1)
+        self.nG = nGroups         # number of energy groups
+        #
         # ord flux vec: 0 is cell centered, 1 is left, 2 is right face
-        self.ordFlux = np.ones((nGroups, 3, len(self.sNords)))
-        self.qin = np.ones((nGroups, 3))
+        self.ordFlux = np.ones((nGroups, 3, self.sNords))
+        self.totOrdFlux = np.zeros((nGroups, 3, self.sNords))
+        self.qin = np.ones((nGroups, 3, self.sNords))  # scatter/fission source computed by scattering source iteration
+        if self.S == 'fission':
+            self.multiplying = True
+        else:
+            self.multiplying = False
+        # Cell can have 3 types of Bcs:
+        #   - Fixed face flux
+        #       - vaccume (incomming flux 0)
+        #       - fixed flux (incomming flux = const)
+        #   - reflecting flux
+        #   - white
+        self.boundaryCond = kwargs.pop('bc', None)  # none denotes interior cell
+        # fixed volumetric source
+        self.S = kwargs.pop('source', np.zeros((nGroups, 3, self.sNords)))
 
-    def _sweepOrd(self, ordinateDirs, initialSource, depth=0):
+    def sweepOrd(self, depth=0):
         """
         Use the scattering source iteration to sweep through sN discrete balance
         equations, one for each sN ordinate direction.
@@ -182,7 +228,7 @@ class Cell1DSn(object):
 
         Note that the RHS does not require any space deriviatives to be computed.
         sum the dummy qfluxes to obtain the true flux.
-        flux = sum(m, qflux^(m))
+        totOrdflux = sum(m, qflux^(m))
 
         As m-> inf.  fewer and fewer neutrons will be around to contribute to the
         mth scattering source.  qflux^(m) should tend to 0 at large m.
@@ -194,18 +240,32 @@ class Cell1DSn(object):
             - :rtype: return type
         """
         # 0th scattering soucre is given (fission or non-mult src term):
-        sIn = initialSource
-        if depth > 1:
-            # Transport equation in sNords directions
-            for oi in ordinateDirs:
-                scatteringSourceVec = self.evalScatter()
+        sIn = np.zeros(np.shape(self.ordFlux))
+        if depth >= 1:
+            for oi in range(self.sNords):
+                scatteringSourceVec = self._evalScatterSource(oi)
                 sIn += np.sum(scatteringSourceVec)
+        elif self.multiplying and depth == 0:
+            sIn = self._computeFissionSource()
+        elif not self.multiplying and depth == 0:
+            sIn = self.S
+        self.qin = sIn
         return sIn
 
-    def evalScatter(self):
+    def _computeFissionSource(self, nuFission, keff):
+        if self.multiplying:
+            # multiplying medium source
+            return (1 / keff) * np.sum(nuFission * self.ordFlux, axis=0)
+            pass
+        else:
+            # need fixed source from user input
+            pass
+
+    def _evalScatterSource(self, oi):
         """
         computes scattering source:
-            sum_n(sigma_s(x, Omega.Omega')*flux_n(r, omega)
+            sigma_s(x, Omega.Omega')*flux_n(r, omega)
+            where n is the group
         returns vector of scattered fluxes (flux after scattering op
         has acted upon it).
         """
@@ -235,18 +295,18 @@ class Cell1DSn(object):
             - :param mu: len N direction cosine vector
             - :type mu: nparray
             - :param oflux: ordinate flux vector
-            - :type mu: nparray
+            - :type oflux: nparray
             - :param wN: ordinate quadrature weight
-            - :type mu: nparray
+            - :type wN: nparray
             - :return: len N vec. legendre poly evaluated at input mu vec, scaled by input weights
             - :rtype: return nparray
         """
         return np.polynomial.legendre.legval(mu, wN * oflux)
 
-    def _sweepEnergy(self, oi):
+    def sweepEnergy(self, oi):
         pass
 
-    def _solveEnergy(self, H, qin, oi):
+    def solveEnergy(self, H, qin, oi):
         """
         Instead of performing down/up-scatter sweeps:
         solve simple ngrp x ngrp Ax = b problem.
@@ -268,3 +328,38 @@ class Cell1DSn(object):
             - :rtype: return type
         """
         self.ordFlux[:, 0, oi] = np.linalg.solve(H, qin)
+
+
+class Sn1Dbc(object):
+    def __init__(self, cell, face, bcType, depth=0):
+        if bcType == 'vac':
+            self.applyVacBC(cell, face)
+        else:
+            pass
+
+    def applyRefBC(cell, face):
+        # reflects cell outgoing flux at boundary to incomming flux
+        # ex: flux_2 == flux_1
+        faceDot = cell.sNmu * cell.faceNormals[face - 1]
+        # look for equal magnitude but opposite sign when assigning direction
+        # pairs
+        directionPairs = []
+        for inwardDir, outwardDir in directionPairs:
+            cell.ordFlux[:, face, inwardDir] = cell.ordFlux[:, face, outwardDir]
+
+    def applyVacBC(cell, face):
+        # sets incomming flux to zero on designated face
+        faceDot = cell.sNmu * cell.faceNormals[face - 1]
+        inwardDirs = np.where(faceDot < 0)
+        cell.ordFlux[:, face, inwardDirs] = 0.0
+
+    def applyFixedFluxBC():
+        # only applied to un-collieded flux iter: depth=0, vac for all
+        # others.
+        # sets incomming flux to user specified val
+        pass
+
+    def applyWhiteBC():
+        # Summs all out going flux and redistributes evenly over all inward
+        # facing directions.
+        pass
