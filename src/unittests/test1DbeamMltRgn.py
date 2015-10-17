cededcf1e2032132e42dad0f026e17a3976e5d04
mcoveri <william.gurecky@gmail.com>
2015-10-16 21:22:48 -0500

    added start of mult region beam test

---
+++ src/unittests/test1DbeamMltRgn.py
@@ -0,0 +1,68 @@
+import unittest
+import numpy as np
+import sn1D as sn
+
+# Load xs database
+import materials.materialMixxer as mx
+mx.genMaterialDict('./materials/newXS')
+
+# plotters
+import plotters.fluxEplot as flxPlt
+import plotters.scalarFluxPlot as sfp
+import plotters.plotOrdFlux as pof
+
+
+class test1Dbeam(unittest.TestCase):
+
+    def testAtten(self):
+        print("\n========= INITIATING MULT REGION BEAM TEST ==========")
+        sNord = 4
+        # define fixed source boundary cond
+        srcStrength = 1.e6  # [n / cm**2-s]
+        # energy distribution of source (all born at 0.1MeV
+        srcEnergy = np.array([0.0, 0, 1.0, 0, 0, 0, 0, 0, 0, 0])
+        #
+        # ## REGION 1 - GRAPHITE ###
+        width1, dX1 = 20.0, 1.0
+        region1Mat = mx.mixedMat({'c12': 1.0})
+        region1Mat.setDensity(2.26)
+        region1mesh1D = sn.Mesh1Dsn([0, width1], dX1, region1Mat, sN=sNord)
+        bcs1 = {0: {'fixN': (1, [srcStrength, srcEnergy])}}
+        region1mesh1D.setBCs(bcs1)
+        # ## REGION 2 - BORATED CARBON ###
+        width2, dX2 = 5.0, 0.1
+        region2Mat = mx.mixedMat({'c12': 0.999, 'b10': 0.001})
+        region2Mat.setDensity(2.2)
+        region2mesh1D = sn.Mesh1Dsn([width1 + dX1, width1 + dX1 + width2], dX2, region2Mat, sN=sNord)
+        bcs2 = {-1: {'vac': (2, 0)}}
+        region2mesh1D.setBCs(bcs2)
+        #
+        # ## BUILD DOMAIN ###
+        domain = sn.SubDomain()
+        domain.addRegion(region1mesh1D)
+        domain.addRegion(region2mesh1D)
+        domain.buildSweepTree()
+        #
+        # ## SWEEP DOMAIN ###
+        for si in range(180):
+            resid = domain.sweepSubDomain(30)
+            if resid < 1e-5:
+                break
+        scalarFlux = domain.getScalarFlux()
+        for g in range(len(srcEnergy)):
+            pass
+        flxPlt.plotFluxE(scalarFlux[-1][::-1])  # flux vs E at left edge
+        # plot ord fluxes at leading edge
+        ordFlux = domain.getOrdFlux()
+        angles = np.arccos(domain.regions[0].cells[0].sNmu)
+        g = 2
+        mag = ordFlux[1][g, 0, :] / sum(ordFlux[1][g, 0, :])
+        pof.compass(angles, mag, figName='polar_grp3')
+        # plot ord fluxes at mid plane
+        g = 3
+        mag = ordFlux[1][g, 0, :] / sum(ordFlux[1][g, 0, :])
+        pof.compass(angles, mag, figName='polar_grp4')
+
+
+if __name__ == "__main__":
+    unittest.main()
