finnally fixed gradient sign issue.  now result is identicle to diamond diff in 1D

src/fe/elements.py
@@ -79,6 +79,7 @@ class InteriorElement(object):
 
     def _computeDeltaX(self):
         self.deltaX = np.abs(self.nodeVs[0] - self.nodeVs[1])
+        self.sortedNodeIndex = np.argsort(self.nodeVs)
 
     def getElemMatrix(self, g, o, totalXs):
         """
@@ -94,9 +95,15 @@ class InteriorElement(object):
         """
         elemIDmatrix = [(self.nodeIDs[0], self.nodeIDs[0]), (self.nodeIDs[0], self.nodeIDs[1]),
                         (self.nodeIDs[1], self.nodeIDs[0]), (self.nodeIDs[1], self.nodeIDs[1])]
-        feI = np.array([[1, -1], [-1, 1]])
-        feI2 = np.array([[1, 0], [0, 1]])
-        elemMatrix = (np.abs(self.sNmu[o]) / self.deltaX) * feI + (2.0 * totalXs[g] * self.deltaX) * feI2
+        if self.nodeVs[0] < self.nodeVs[1]:
+            #TODO: in 2 and 3D we will need to use the sortedNodeIndex to get sign on the grad
+            # term correct! but in 1D, just comparing the node X locations is
+            # good enough.
+            feI = np.array([[-1, 1], [-1, 1]])
+        else:
+            feI = np.array([[1, -1], [1, -1]])
+        feI2 = np.array([[1, 0.5], [0.5, 1]])
+        elemMatrix = (0.5 * self.sNmu[o]) * feI + ((1 / 3.) * totalXs[g] * self.deltaX) * feI2
         return elemIDmatrix, elemMatrix.flatten()
 
     def getRHS(self, g, o):
@@ -104,7 +111,7 @@ class InteriorElement(object):
         Produces right hand side of neutron balance for this element.
         """
         elemIDRHS = np.array([self.nodeIDs[0], self.nodeIDs[1]])
-        elemRHS = 2.0 * self.deltaX * np.array([self.qin[g, o], self.qin[g, o]])
+        elemRHS = 0.5 * self.deltaX * np.array([self.qin[g, o], self.qin[g, o]])
         return elemIDRHS, elemRHS
 
     def resetTotOrdFlux(self):
@@ -128,14 +135,16 @@ class InteriorElement(object):
         weights = np.array([np.zeros(self.maxLegOrder + 1)])
         lw = np.arange(self.maxLegOrder + 1)
         if depth >= 1:
-            if depth >= 2:
-                for g in range(self.nG):
-                    self.qin[g, :] = overRlx * (self.evalScatterSourceImp(g, skernel, weights, lw) - self.previousQin[g, :]) + self.previousQin[g, :]
-                self.previousQin = self.qin
-            else:
+            #if depth >= 2:
+            #    for g in range(self.nG):
+            #        self.qin[g, :] = overRlx * (self.evalScatterSourceImp(g, skernel, weights, lw) - self.previousQin[g, :]) + self.previousQin[g, :]
+            #    self.previousQin = self.qin
+            #else:
+            #    for g in range(self.nG):
+            #        self.qin[g, :] = self.evalScatterSourceImp(g, skernel, weights, lw)
+            #    self.previousQin = self.qin
                 for g in range(self.nG):
                     self.qin[g, :] = self.evalScatterSourceImp(g, skernel, weights, lw)
-                self.previousQin = self.qin
         elif self.multiplying and depth == 0:
             for g in range(self.nG):
                 # compute gth group fission source
@@ -220,7 +229,7 @@ class BoundaryElement(object):
             if self.bcData.shape != self.parent.centTotFlux.shape:
                 print("WARNING: BC flux shape mismatch.")
             if depth == 0:
-                return self.diricletBC(RHS)
+                return self.dirichletBC(RHS)
             else:
                 return self.vacBC(RHS)
         else:
@@ -307,7 +316,7 @@ class BoundaryElement(object):
         RHS[:, self.inOs, self.nodeIDs] = self.bankedRefFlux[:, self.inOs, self.internalBCnodeIDs]
         return RHS
 
-    def diricletBC(self, RHS):
+    def dirichletBC(self, RHS):
         """
         Fixed flux bc.
         """

src/spyTran.py
@@ -39,7 +39,7 @@ def initSolve(geoFile, materialDict, bcDict, srcDict, **kwargs):
     return solver
 
 
-def nonMultiplying(slv):
+def nonMultiplying(slv, residTol=0.5e-5):
     """
     Pseudo Code non mult solve:
         1. Init problem (see initSolve)
@@ -59,7 +59,7 @@ def nonMultiplying(slv):
     print("====================================================================")
     print(" ||ScFlux||/||TotFlux|| -- Lin Solve Time [s] -- Scattering Time [s]")
     print("====================================================================")
-    for i in range(0, 50):
+    for i in range(0, 180):
         slv.scatterSource()
         norms, times = slv.solveFlux()
         totScTime += times[0]
@@ -67,6 +67,8 @@ def nonMultiplying(slv):
         print("       " + "{:.4e}".format(norms) + "              " +
               "{:.2e}".format(times[1]) + "           " +
               "{:.2e}".format(times[0]))
+        if norms < residTol:
+            break
     print("====================================================================")
     print("    TOTAL SOLVE TIME [S]       " + "{:.2e}".format(totLsTime) + "           " +
           "{:.2e}".format(totScTime))
@@ -96,17 +98,19 @@ def multiplying():
 
 if __name__ == "__main__":
     # Solver settings
-    sN, nG = 8, 10
+    sN, nG = 16, 10
     # Geometry
     geoFile = 'utils/testline2.geo'
     # Materials
-    material1 = mx.mixedMat({'h1': 3.35e22 / 1e24, 'o16': 1.67e22 / 1e24})
-    material2 = mx.mixedMat({'h1': 3.35e22 / 1e24, 'o16': 1.67e22 / 1e24, 'b10': 1.e5 / 1e24})
-    materialDict = {'mat_1': material1,
-                    'mat_2': material2}
+    #material1 = mx.mixedMat({'h1': 3.35e22 / 1e24, 'o16': 1.67e22 / 1e24})
+    #material2 = mx.mixedMat({'h1': 3.35e22 / 1e24, 'o16': 1.67e22 / 1e24, 'b10': 1.e5 / 1e24})
+    attnMat = mx.mixedMat({'c12': 1.0})
+    attnMat.setDensity(2.24)
+    materialDict = {'mat_1': attnMat,
+                    'mat_2': attnMat}
     # Boundary conditions
     fixedFlux1 = np.zeros((nG, sN))
-    fixedFlux1[1, 0] = 1e10    # 1e10 flux in ord 0, grp 1
+    fixedFlux1[0, 0] = 16 * 1e6    # 1e6 flux in ord 0, grp 1
     bcDict = {'bc1': fixedFlux1,
               'bc2': 'vac'}
     # Volumetric sources
@@ -116,6 +120,13 @@ if __name__ == "__main__":
     nonMultiplying(slv)
     slv.writeData('1Dtestout.h5')
     plotter = fe1Dplt('1Dtestout.h5')
+    plotter.plotScalarFlux(0)
     plotter.plotScalarFlux(1)
     plotter.plotScalarFlux(2)
     plotter.plotScalarFlux(3)
+    plotter.plotScalarFlux(4)
+    plotter.plotScalarFlux(5)
+    plotter.plotScalarFlux(6)
+    plotter.plotScalarFlux(7)
+    plotter.plotScalarFlux(8)
+    plotter.plotScalarFlux(9)