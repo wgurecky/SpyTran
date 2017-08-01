1abdd0a3b4f8d363236deb7a55bfcba368e6414c
hephaestus <william.gurecky@gmail.com>
2017-08-01 18:50:32 -0500

    adding 1d mesh element neighbor finding routine

--- spytran/utils/gmshPreproc.py
+++ spytran/utils/gmshPreproc.py
@@ -7,7 +7,7 @@
 # TODO: 3D meshs
 #
 #
-
+from __future__ import division
 from collections import defaultdict
 import subprocess
 import re
@@ -15,14 +15,24 @@ import sys
 import numpy as np
 
 
+# ============================================================================ #
 class gmshMesh(object):
+    """!
+    @brief Parses 1D and 2D inp files from GMSH.
+    Only supports triangular elements in 2D!
+    """
     def __init__(self, geoFile, inpFileName=None):
+        """!
+        @param geoFile  Input GMSH compatible geo file.
+        @param inpFileName  Custom name for .inp gmsh output (optional)
+        """
         if not inpFileName:
             self.inpFileName = geoFile + '.inp'
         else:
             self.inpFileName = inpFileName + '.inp'
         self.geoFile = geoFile
         self.parseGEO()
+        self.dg_element_dict = {}
 
     def runGMSH(self, dim=1):
         self.dim = dim
@@ -214,21 +224,149 @@ class gmshMesh(object):
                 pass
         return bEdict
 
+    def enable_connectivity(self):
+        """!
+        @brief Builds shared edges dict and global DG
+        element mesh.  Verticies are multiply defined in this scheme.
+        Note: Very expensive method (N^2).
+        Must be manually called since mesh connectivity is not always desired.
+        """
+        interior_mesh_elements = []
+        for regionID, region in self.regions.iteritems():
+            try:
+                interior_mesh_elements.append(self.regions[regionID]['elements'])
+            except:
+                pass
+        interior_mesh_elements = np.concatenate(interior_mesh_elements, axis=0)
+        dg_element_dict = self._build_global_dg_mesh(interior_mesh_elements)
+        # find neighbors
+        tmp_edge_list = self._edge_list(dg_element_dict)
+        #
+        for ele_id, ele in dg_element_dict.iteritems():
+            ele_neighbors, ele_edge_neighbors = \
+                    self._find_edge_neighbors(ele_id, dg_element_dict, tmp_edge_list)
+            dg_element_dict[ele_id]['neighbors'] = {'elements': ele_neighbors,
+                                                    'edges': ele_edge_neighbors}
+        self.dg_element_dict = dg_element_dict
 
+    def _build_global_dg_mesh(self, interior_mesh_elements):
+        """!
+        @brief Generates a dictionary of all elements in the mesh.
+        Each element is asscoiated with bounding edges and verticies.
+        @return dictionary of elements
+        """
+        dg_element_dict = {}
+        global_node_id_idx = 0
+        edge_id = 0
+        for ele in interior_mesh_elements:
+            el_id = int(ele[0])
+            dg_element_dict[el_id] = {'gmsh_nodeIDs': ele[1:]}
+            dg_element_dict[el_id]['vertex_pos'] = []
+            for gmsh_node_id in ele[1:]:
+                dg_element_dict[el_id]['vertex_pos'].append( \
+                        self.nodes[int(np.argwhere(gmsh_node_id == self.nodes[:, 0]))][1:])
+            dg_element_dict[el_id]['vertex_pos'] = \
+                np.array(dg_element_dict[el_id]['vertex_pos'])
+            # dg_element_dict[el_id]['nodePos'] =
+            element_global_node_ids = np.zeros(len(ele[1:]), dtype=int)
+            local_global_node_ids = np.zeros(len(ele[1:]), dtype=int)
+            local_node_id_idx = 0
+            for i, nodeID in enumerate(ele[1:]):
+                element_global_node_ids[i] = global_node_id_idx
+                local_global_node_ids[i] = local_node_id_idx
+                global_node_id_idx += 1
+                local_node_id_idx += 1
+            dg_element_dict[el_id]['global_nodeIDs'] = element_global_node_ids
+            dg_element_dict[el_id]['local_nodeIDs'] = local_global_node_ids
+            dg_element_dict[el_id]['centroid'] = \
+                    np.sum(dg_element_dict[el_id]['vertex_pos'], axis=0) / \
+                    len(dg_element_dict[el_id]['vertex_pos'])
+            # label edges
+            dg_element_dict[el_id]['edges'] = {}
+            if self.dim == 1:
+                for edge_pos, edge_id in zip(dg_element_dict[el_id]['vertex_pos'],
+                                             dg_element_dict[el_id]['global_nodeIDs']):
+                    dg_element_dict[el_id]['edges'][edge_id] = {'edge_node_ids': (edge_id,),
+                                                                'edge_centroid': edge_pos}
+                    ele_centroid = dg_element_dict[el_id]['centroid']
+                    dg_element_dict[el_id]['edges'][edge_id]['edge_normal'] = \
+                            (edge_pos - ele_centroid) / np.linalg.norm(edge_pos - ele_centroid)
+                    edge_id += 1
+            elif self.dim == 2:
+                # TODO: FIX 2D CASE
+                for i in range(len(dg_element_dict[el_id]['global_nodeIDs'])):
+                    dg_element_dict[el_id]['edges'][edge_id] = {'edge_node_ids': [],
+                                                                'edge_centroid': 0.}
+                    try:
+                        edge_node_id_pair = (dg_element_dict[el_id]['global_nodeIDs'][i], \
+                            dg_element_dict[el_id]['global_nodeIDs'][i+1])
+                    except:
+                        edge_node_id_pair = (dg_element_dict[el_id]['global_nodeIDs'][i], \
+                            dg_element_dict[el_id]['global_nodeIDs'][0])
+                    dg_element_dict[el_id]['edges']['edge_node_ids'].append(edge_node_id_pair)
+                    # dg_element_dict[el_id]['edges']['edge_centroids'].append()
+                    edge_id += 1
+            else:
+                raise RuntimeError("Dim must be 1 or 2")
+        return dg_element_dict
+
+    def _edge_list(self, dg_element_dict):
+        """!
+        @brief Generates a flat edge list view from dict
+        @return list of edges
+        """
+        edge_list = []
+        for ele_id, ele in dg_element_dict.iteritems():
+            for edge_id, edge in ele['edges'].iteritems():
+                edge_list.append([ele_id, edge_id,
+                                  edge['edge_centroid'][0],
+                                  edge['edge_centroid'][1],
+                                  edge['edge_centroid'][2]])
+        return np.array(edge_list)
+
+    def _find_edge_neighbors(self, ele_id, dg_element_dict, edge_list):
+        """!
+        @brief Find the neighbors of a given element by
+        shared edge inspection.
+        @param ele_id  global element id to inspect for neighbors
+        @param dg_element_dict  element dictionary
+        @param edge_list  list of edges
+        @return neighboring elements, neighboring edges
+        """
+        edge_mask = (edge_list[:, 0] != ele_id)
+        neighbor_list = []
+        neighbor_edge_list = []
+        for edge_id, edge in dg_element_dict[ele_id]['edges'].iteritems():
+            # compute the distance to each other edge centroid in the mesh
+            edge_dists = np.linalg.norm(edge_list[edge_mask][:, 2:] - edge['edge_centroid'], axis=1)
+            nearest_edge_idx = np.argmin(edge_dists)
+            if edge_dists[nearest_edge_idx] == 0.:
+                nearest_ele_id = int(edge_list[edge_mask][nearest_edge_idx][0])
+                nearest_ele_shared_edge_id = int(edge_list[edge_mask][nearest_edge_idx][1])
+                neighbor_list.append(nearest_ele_id)
+                neighbor_edge_list.append(nearest_ele_shared_edge_id)
+        return neighbor_list, neighbor_edge_list
+
+
+    @property
+    def region_ids(self):
+        """!
+        @brief Regions ids list
+        """
+        return list(self.regions.keys())
+
+
+# ============================================================================ #
 class gmsh1DMesh(gmshMesh):
     def __init__(self, **kwargs):
         super(gmsh1DMesh, self).__init__(**kwargs)
         self.runGMSH(1)
 
     def gmsh1Dparse(self):
-        """
-        Create regions and elements from .inp file gmsh output file
-        Each region should be ascossiated with a material using the
-        .geo input file.
-        """
         pass
 
 
+# ============================================================================ #
 class gmsh2DMesh(gmshMesh):
     def __init__(self, **kwargs):
         super(gmsh2DMesh, self).__init__(**kwargs)
@@ -238,6 +376,7 @@ class gmsh2DMesh(gmshMesh):
         pass
 
 
+# ============================================================================ #
 def fileToList(infile):
     '''
     list generator helper function so we only need to read the file
@@ -272,3 +411,5 @@ def checkLine(line, reFlags):
 
 if __name__ == "__main__":
     Mesh1D = gmsh1DMesh(geoFile='testline2.geo')
+    Mesh1D.enable_connectivity()
+    pass
